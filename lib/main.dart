f520e34df21f16c2e070475a2677a11d14b00827
Leiner <leinersacdalan4@gmail.com>
2023-06-01 16:53:47 +0800

    step-2

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:practice_app/firebase_options.dart';
+import 'package:practice_app/view/login_view.dart';
+import 'package:practice_app/view/register_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   runApp(
-    const MaterialApp(
+     MaterialApp(
       title: 'Flutter Demo',
-      home: Homepage(),
+      home: const Homepage(),
+      routes: {
+        '/login/': (context) => const LoginView(),
+        '/register/': (context) => const RegisterView()
+      },
     ),
   );
 }
@@ -17,29 +22,24 @@ class Homepage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Home'),
-      ),
-      body: FutureBuilder(
+    return FutureBuilder(
         future: Firebase.initializeApp(
           options: DefaultFirebaseOptions.currentPlatform,
         ),
         builder: (context, snapshot) {
           switch (snapshot.connectionState) {
             case ConnectionState.done:
-              final user = FirebaseAuth.instance.currentUser;
-              if (user?.emailVerified ?? false){
-                print('you are verfied user');
-              }else{
-                print('you are not verified user');
-              }
-              return const Text('Done');
-              default:
-              return const Text('Loading....');
+              // final user = FirebaseAuth.instance.currentUser;
+              // if (user?.emailVerified ?? false) {
+              // return const Text('Done');
+              // } else {
+              //   return const VerifyEmailView();
+              // }
+              return const LoginView();
+            default:
+              return const CircularProgressIndicator();
           }
         },
-      ),
-    );
+      );
   }
 }
