3efaec562cfca6d2b950ed1f2e801203d07b7a40
Leiner <leinersacdalan4@gmail.com>
2023-06-03 03:39:12 +0800

    step-8

--- lib/view/login_view.dart
+++ lib/view/login_view.dart
@@ -62,12 +62,23 @@ class _LoginViewState extends State<LoginView> {
                   email: email,
                   password: password,
                 );
-                Future.delayed(Duration.zero, () {
-                  Navigator.of(context).pushNamedAndRemoveUntil(
-                    notesRoute,
-                    (route) => false,
-                  );
-                });
+                final user = FirebaseAuth.instance.currentUser;
+                if (user?.emailVerified ?? false) {
+                  Future.delayed(Duration.zero, () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      notesRoute,
+                      (route) => false,
+                    );
+                  });
+                } else {
+                  
+                  Future.delayed(Duration.zero, () {
+                    Navigator.of(context).pushNamedAndRemoveUntil(
+                      verifyEmailRoute,
+                      (route) => false,
+                    );
+                  });
+                }
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'user-not-found') {
                   await showErrorDialog(
