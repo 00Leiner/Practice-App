36c2ca3476015f9a066a28e19c3c92e9bbf77e70
Leiner <leinersacdalan4@gmail.com>
2023-06-06 16:15:41 +0800

    step-17

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:practice_app/constants/routes.dart';
 import 'package:practice_app/services/auth/auth_service.dart';
 import 'package:practice_app/view/login_view.dart';
-import 'package:practice_app/view/notes/new_note_view.dart';
+import 'package:practice_app/view/notes/create_update_note_view.dart';
 import 'package:practice_app/view/notes/notes_view.dart';
 import 'package:practice_app/view/register_view.dart';
 import 'package:practice_app/view/verify_email_view.dart';
@@ -21,7 +21,7 @@ void main() {
         registerRoute: (context) => const RegisterView(),
         notesRoute: (context) => const NotesView(),
         verifyEmailRoute: (context) => const VerifyEmailView(),
-        newNoteRoute: (context) => const NewNoteView(),
+        createOrUpdateNoteRoute: (context) => const CreateUpdateNoteView(),
       },
     ),
   );
