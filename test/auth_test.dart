a558189260fefabe3d450b8e1fa4128c24ce51fa
Leiner <leinersacdalan4@gmail.com>
2023-06-07 12:06:36 +0800

    step-19

--- test/auth_test.dart
+++ test/auth_test.dart
@@ -1,5 +1,5 @@
-import 'package:practice_app/services/auth/auth_provider.dart';
 import 'package:practice_app/services/auth/auth_exceptions.dart';
+import 'package:practice_app/services/auth/auth_provider.dart';
 import 'package:practice_app/services/auth/auth_user.dart';
 import 'package:test/test.dart';
 
@@ -116,6 +116,7 @@ class MockAuthProvider implements AuthProvider {
     if (email == 'foo@bar.com') throw UserNotFoundAuthException();
     if (password == 'foobar') throw WrongPasswordAuthException();
     const user = AuthUser(
+      id: 'my_id',
       isEmailVerified: false,
       email: 'foo@bar.com',
     );
@@ -137,6 +138,7 @@ class MockAuthProvider implements AuthProvider {
     final user = _user;
     if (user == null) throw UserNotFoundAuthException();
     const newUser = AuthUser(
+      id: 'my_id',
       isEmailVerified: true,
       email: 'foo@bar.com',
     );
